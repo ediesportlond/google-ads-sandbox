3297e4b9e67be264359f9b5fdd6404f63cd4abef
Eddie Espinosa <espinosa.edilson@gmail.com>
2023-02-05 19:31:50 -0500

    working on query

--- src/Test.php
+++ src/Test.php
@@ -2,9 +2,11 @@
 
 use Google\Ads\GoogleAds\Lib\OAuth2TokenBuilder;
 use Google\Ads\GoogleAds\Lib\V10\GoogleAdsClientBuilder;
+use GPBMetadata\Google\Ads\GoogleAds\V10\Enums\CriterionType;
+use GPBMetadata\Google\Ads\GoogleAds\V10\Enums\KeywordMatchType;
 
 require __DIR__ . "/../vendor/autoload.php";
-require __DIR__. "/../secrets.php";
+require __DIR__ . "/../secrets.php";
 
 class TEST
 {
@@ -21,7 +23,42 @@ class TEST
     // Make a request to the Google Ads API.
     $googleAdsServiceClient = $googleAdsClient->getGoogleAdsServiceClient();
 
-    print_r($googleAdsServiceClient);
+    // print_r($googleAdsServiceClient);
+
+    $query =
+      'SELECT ad_group.id, '
+      . 'ad_group_criterion.type, '
+      . 'ad_group_criterion.criterion_id, '
+      . 'ad_group_criterion.keyword.text, '
+      . 'ad_group_criterion.keyword.match_type '
+      . 'FROM ad_group_criterion '
+      . 'WHERE ad_group_criterion.type = KEYWORD';
+
+    $response =
+      $googleAdsServiceClient->search(CUSTOMER_ID, $query, ['pageSize' => 10]);
+
+    foreach ($response->iterateAllElements() as $googleAdsRow) {
+
+      $resourceNameString = printf(
+        " and resource name '%s'",
+        $googleAdsRow->getAdGroup()->getResourceName()
+      );
+
+      printf(
+        "Keyword with text '%s', match type '%s', criterion type '%s', and ID %d "
+        . "was found in ad group with ID %d%s.%s",
+        $googleAdsRow->getAdGroupCriterion()->getKeyword()->getText(),
+        KeywordMatchType::name( 
+            $googleAdsRow->getAdGroupCriterion()->getKeyword()->getMatchType()
+        ),
+        CriterionType::name($googleAdsRow->getAdGroupCriterion()->getType()),
+        $googleAdsRow->getAdGroupCriterion()->getCriterionId(),
+        $googleAdsRow->getAdGroup()->getId(),
+        $resourceNameString,
+        PHP_EOL
+    );
+
+    }
 
   }
 }
