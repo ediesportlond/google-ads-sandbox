0dca6ea9bb094f4205ecae11324cc585182d4ca8
Eddie Espinosa <espinosa.edilson@gmail.com>
2023-02-05 19:09:09 -0500

    succesful api call

--- src/Test.php
+++ src/Test.php
@@ -1,10 +1,28 @@
 <?php
 
-require '../vendor/autoload.php';
+use Google\Ads\GoogleAds\Lib\OAuth2TokenBuilder;
+use Google\Ads\GoogleAds\Lib\V10\GoogleAdsClientBuilder;
 
-class TEST{
-  public static function main(){
-    echo "Hello World";
+require __DIR__ . "/../vendor/autoload.php";
+require __DIR__. "/../secrets.php";
+
+class TEST
+{
+  public static function main()
+  {
+
+    $oAuth2Credential = (new OAuth2TokenBuilder())->fromFile(CONFIG_PATH)->build();
+
+    $googleAdsClient = (new GoogleAdsClientBuilder())
+      ->fromFile(CONFIG_PATH)
+      ->withOAuth2Credential($oAuth2Credential)
+      ->build();
+
+    // Make a request to the Google Ads API.
+    $googleAdsServiceClient = $googleAdsClient->getGoogleAdsServiceClient();
+
+    print_r($googleAdsServiceClient);
+
   }
 }
 
